d2ea8e5fcf0d9e5c3fc7e66588e0c4b425925caa
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-01-03 13:41:14 +0300

    Autofac module added

---
+++ Lessons/Fundamentals/DepedencyInjection/Autofac/MyModule.cs
@@ -0,0 +1,51 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace DepedencyInjection
+{
+    [DependsOn(typeof(AbpAutofacModule))]
+    public class MyModule : AbpModule
+    {
+
+    }
+}
+#region ASP.NET Core Application
+/*
+    public class Startup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication<MyWebModule>(options =>
+            {
+                //Integrate Autofac!
+                options.UseAutofac();
+            });
+
+            return services.BuildServiceProviderFromFactory();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.InitializeApplication();
+        }
+    }
+*/
+#endregion
+
+#region Console Application
+/*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<AppModule>(options =>
+            {
+                options.UseAutofac(); //Autofac integration
+            }))
+            {
+                //...
+            }
+        }
+    }
+*/
+#endregion
