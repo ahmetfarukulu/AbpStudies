85fae497d99a9f3601cfd208ab8d1193b3c42745
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-01-27 16:15:00 +0300

    Domain: DataSeederContributor added

---
+++ Allegory.Sample/Allegory.Sample.Domain/SampleDataSeederContributor.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+
+namespace Allegory.Sample
+{
+    public class SampleDataSeederContributor : IDataSeedContributor, ITransientDependency
+    {
+        public SampleDataSeederContributor()
+        {
+            //dependency inject repository
+        }
+        public Task SeedAsync(DataSeedContext context)
+        {
+            //seed data
+
+            return Task.CompletedTask;
+        }
+    }
+}
