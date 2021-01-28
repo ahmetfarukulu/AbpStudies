3e0dc9456f85eabe867b7449feb31873cc71d5fc
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-01-28 10:21:14 +0300

    Domain: AppUser added

---
+++ Allegory.Sample/Allegory.Sample.Domain/Users/AppUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Users;
+
+
+namespace Allegory.Sample.Users
+{
+    public class AppUser : FullAuditedAggregateRoot<Guid>, IUser
+    {
+        #region Base properties
+        public virtual Guid? TenantId { get; private set; }
+
+        public virtual string UserName { get; private set; }
+
+        public virtual string Name { get; private set; }
+
+        public virtual string Surname { get; private set; }
+
+        public virtual string Email { get; private set; }
+
+        public virtual bool EmailConfirmed { get; private set; }
+
+        public virtual string PhoneNumber { get; private set; }
+
+        public virtual bool PhoneNumberConfirmed { get; private set; }
+        #endregion
+        
+        //Custom properties here
+
+        private AppUser()
+        {
+
+        }
+    }
+}
