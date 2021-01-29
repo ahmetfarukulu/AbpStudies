46ab2ea4edeefc878337450c7f7220d2bff39c9f
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-01-29 17:29:00 +0300

    EntityFrameworkCore.DbMigrator: Migration added DbMigrator: Module added

---
+++ Allegory.Sample/src/Allegory.Sample.DbMigrator/SampleDbMigratorModule.cs
@@ -0,0 +1,20 @@
+﻿using Allegory.Sample.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace Allegory.Sample.DbMigrator
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(SampleEntityFrameworkCoreDbMigrationsModule)
+        //typeof(SampleApplicationContractsModule) => Create AppContract project
+        )]
+    public class SampleDbMigratorModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+        }
+    }
+}
