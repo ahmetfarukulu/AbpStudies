b9d7d427ee5719e3209d37421680959774478cc7
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-01-29 17:01:48 +0300

    EntityFrameworkCore.DbMigration: DbContext,module added

---
+++ Allegory.Sample/src/Allegory.Sample.EntityFrameworkCore.DbMigrations/EntityFrameworkCore/EntityFrameworkCoreSampleDbSchemaMigrator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Allegory.Sample.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.DependencyInjection;
+
+namespace Allegory.Sample.EntityFrameworkCore
+{
+    public class EntityFrameworkCoreSampleDbSchemaMigrator : ISampleDbSchemaMigrator, ITransientDependency
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public EntityFrameworkCoreSampleDbSchemaMigrator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task MigrateAsync()
+        {
+            await _serviceProvider
+                .GetRequiredService<SampleMigrationsDbContext>()
+                .Database
+                .MigrateAsync();
+        }
+    }
+}
---
+++ Allegory.Sample/src/Allegory.Sample.EntityFrameworkCore.DbMigrations/EntityFrameworkCore/SampleMigrationsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.IdentityServer.EntityFrameworkCore;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.TenantManagement.EntityFrameworkCore;
+
+namespace Allegory.Sample.EntityFrameworkCore
+{
+    public class SampleMigrationsDbContext : AbpDbContext<SampleMigrationsDbContext>
+    {
+        public SampleMigrationsDbContext(DbContextOptions<SampleMigrationsDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ConfigurePermissionManagement();
+            builder.ConfigureSettingManagement();
+            builder.ConfigureBackgroundJobs();
+            builder.ConfigureAuditLogging();
+            builder.ConfigureIdentity();
+            builder.ConfigureIdentityServer();
+            builder.ConfigureFeatureManagement();
+            builder.ConfigureTenantManagement();
+
+            builder.ConfigureSample();
+        }
+    }
+}
