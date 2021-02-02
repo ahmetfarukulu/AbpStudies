b6be9f9439f6089ab1168f250780e487da9217c9
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-02-02 13:41:18 +0300

    HttpApi: Module, base contoller added

---
+++ Allegory.Sample/src/Allegory.Sample.HttpApi/Controllers/SampleContoller.cs
@@ -0,0 +1,13 @@
+﻿using Allegory.Sample.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Allegory.Sample.Controllers
+{
+    public abstract class SampleContoller : AbpController
+    {
+        protected SampleContoller()
+        {
+            LocalizationResource = typeof(SampleResource);
+        }
+    }
+}
