6e7f46dc1100357e43cbd18f17194937a912fddb
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-02-03 12:48:52 +0300

    DomainShared: Product permissions added

--- Allegory.Sample/src/Allegory.Sample.Application.Contracts/Permissions/SamplePermissionDefinitionProvider.cs
+++ Allegory.Sample/src/Allegory.Sample.Application.Contracts/Permissions/SamplePermissionDefinitionProvider.cs
@@ -9,7 +9,13 @@ namespace Allegory.Sample.Permissions
         public override void Define(IPermissionDefinitionContext context)
         {
             var sampleGroup = context.AddGroup(SamplePermissions.GroupName);
+
+            var productPermission = sampleGroup.AddPermission(SamplePermissions.Products.Default, L("Permission:Products"));
+            productPermission.AddChild(SamplePermissions.Products.Create, L("Permission:Products.Create"));
+            productPermission.AddChild(SamplePermissions.Products.Edit, L("Permission:Products.Edit"));
+            productPermission.AddChild(SamplePermissions.Products.Delete, L("Permission:Products.Delete"));
+
         }
-        public static LocalizableString L(string name)=> LocalizableString.Create<SampleResource>(name);
+        public static LocalizableString L(string name) => LocalizableString.Create<SampleResource>(name);
     }
 }
--- Allegory.Sample/src/Allegory.Sample.Application.Contracts/Permissions/SamplePermissions.cs
+++ Allegory.Sample/src/Allegory.Sample.Application.Contracts/Permissions/SamplePermissions.cs
@@ -3,5 +3,13 @@
     public class SamplePermissions
     {
         public const string GroupName = "Sample";
+
+        public static class Products
+        {
+            public const string Default = GroupName + ".Products";
+            public const string Create = Default + ".Create";
+            public const string Edit = Default + ".Edit";
+            public const string Delete = Default + ".Delete";
+        }
     }
 }
