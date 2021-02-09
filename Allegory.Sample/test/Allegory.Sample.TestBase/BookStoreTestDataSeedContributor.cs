4db163a4384847d0596dc03e94686f4c7fb3827b
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-02-09 16:22:16 +0300

    TestBase: Module added

---
+++ Allegory.Sample/test/Allegory.Sample.TestBase/BookStoreTestDataSeedContributor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+
+namespace Allegory.Sample
+{
+    public class SampleTestDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        public Task SeedAsync(DataSeedContext context)
+        {
+            /* Seed additional test data... */
+
+            return Task.CompletedTask;
+        }
+    }
+}
