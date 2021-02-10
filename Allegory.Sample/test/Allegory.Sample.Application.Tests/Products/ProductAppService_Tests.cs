82d44cd6d4619597d9239519a0ba7fb68b41a5ee
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-02-10 15:05:28 +0300

    Application.Tests project added

---
+++ Allegory.Sample/test/Allegory.Sample.Application.Tests/Products/ProductAppService_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace Allegory.Sample.Products
+{
+    public class ProductAppService_Tests : SampleApplicationTestBase
+    {
+        private readonly IProductAppService _productAppService;
+
+        public ProductAppService_Tests()
+        {
+            _productAppService = GetRequiredService<IProductAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Not_Create_A_Product_Without_Code()
+        {
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _productAppService.CreateAsync(
+                    new CreateProductDto
+                    {
+                        Code = ""
+                    }
+                );
+            });
+
+            exception.ValidationErrors.ShouldContain(err => err.MemberNames.Any(m => m == "Code"));
+        }
+
+        [Fact]
+        public async Task Should_Create_A_Valid_Product()
+        {
+            var result = await _productAppService.CreateAsync(
+                new CreateProductDto
+                {
+                  Code="P-2",
+                  Name="Product-2"
+                }
+            );
+
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Code.ShouldBe("P-2");
+        }
+    }
+}
---
+++ Allegory.Sample/test/Allegory.Sample.Application.Tests/SampleApplicationTestBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Allegory.Sample
+{
+    public abstract class SampleApplicationTestBase : SampleTestBase<SampleApplicationTestModule>
+    {
+
+    }
+}
